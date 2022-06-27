8f5d56dc6455588baa1849a068d6014b6f55ffde
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-06-27 15:42:19 +0530

    backend

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -26,3 +26,32 @@ export const ADD_PROJECT = gql`
     }
   }
 `;
+
+export const DELETE_PROJECT = gql`
+  mutation DeleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id
+    }
+  }
+`;
+
+export const UPDATE_PROJECT = gql`
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+    }
+  }
+`;
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { GET_PROJECT } from "../queries/projectQueries";
 import ClientInfo from "../components/ClientInfo";
+import EditProjectForm from "./EditProjectForm";
+import DeleteProject from "../components/DeleteProject";
 
 const Project = () => {
   const { id } = useParams();
@@ -23,6 +25,8 @@ const Project = () => {
           <h5 className="mt-3">Project status</h5>
           <p className="lead">{data.project.status}</p>
           <ClientInfo client={data.project.client} />
+          <EditProjectForm project={data.project} />
+          <DeleteProject projectId={data.project.id} />
         </div>
       )}
     </>
