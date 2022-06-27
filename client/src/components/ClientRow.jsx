ac29a5a7533c2f52beeb3ac1947989c40ca5bc36
PoojaSivakumar97 <58037293+poojaSivakumar97@users.noreply.github.com>
2022-06-27 15:50:30 +0530

    final version

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,22 +2,24 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 const ClientRow = ({ client }) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }], ---one way of updating
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    // ---one way of updating
     //another way by using cache
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({
-        query: GET_CLIENTS,
-      });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({
+    //     query: GET_CLIENTS,
+    //   });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
   return (
     <>
